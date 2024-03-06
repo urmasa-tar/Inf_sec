af97e05165b35383ddb3019b6787a99addf91b0e
urmasa-tar <nik.goltsev.97@bk.ru>
2024-03-06 15:24:08 +0700

    Avg_push

--- bash_prog/osdetech.sh
+++ bash_prog/osdetech.sh
@@ -4,7 +4,15 @@
 
 # The main aim of programm - detect os of the system
 
+OS = ""
+
 if type -t wevtutil &> /dev/null
 then 
-	OS = MSWin
-elif type = 
+	let OS = MSWin
+elif type -t scutil &> /dev/null
+then
+	let OS = macOS
+else
+	let Os = Linux
+fi
+echo $OS
